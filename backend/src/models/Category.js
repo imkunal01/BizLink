90e307c021a616b99b0e8262f4b2985955cb9f90
imkunal01 <142741387+imkunal01@users.noreply.github.com>
2025-11-10 12:30:28 +0530

    added mail serivece and working on a payment gateway

--- backend/src/models/Category.js
+++ backend/src/models/Category.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const categorySchema =  new mongoose.schema({
-    name: { type: String, required: true, unique: true, trim: true},
-    slug: { type: String, required: true, unique: true, index: true},
-    description: { type: String, default: ""},
-}, {timestamps: true}
+const categorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true, trim: true },
+    slug: { type: String, required: true, unique: true, index: true },
+    description: { type: String, default: "" },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
